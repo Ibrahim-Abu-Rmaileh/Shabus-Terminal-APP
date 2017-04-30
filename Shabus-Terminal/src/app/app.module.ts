c036ae38c73b398db498621c4f79343e1eea199b
Shai Lehmann <Shai Lehmann>
2017-04-30 21:17:10 +0300

    Create new component for the header of the app

--- Shabus-Terminal/src/app/app.module.ts
+++ Shabus-Terminal/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyHeaderComponent } from './my-header/my-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHeaderComponent
   ],
   imports: [
     BrowserModule,
