983b1fa1d202ca0d31dec0dc18433e34bb14adcf
Shai Lehmann <Shai Lehmann>
2017-05-15 18:49:03 +0300

    First design of navmanu

--- Shabus-Terminal/src/app/my-client/my-client.component.ts
+++ Shabus-Terminal/src/app/my-client/my-client.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyClientComponent implements OnInit {
   clientCounter: number;
+  phoneNumber="054-1234567";
   constructor() {
     this.clientCounter=1;
    }
@@ -21,6 +22,13 @@ export class MyClientComponent implements OnInit {
 removeClient(){
   if(this.clientCounter>1){
     this.clientCounter--;
+  }
+}
+isShabusMember(input){
+  console.log(input.target.value);
+  console.log(this.phoneNumber);
+  if(input.target.value===this.phoneNumber){
+    console.log("Client is shabus member");
   }
 }
 
