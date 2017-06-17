5aa4feb896a38b9625ba6f0ce34a9b18a5e7f48e
yonatan-y <yonatan.yadid1@gmail.com>
2017-06-17 17:04:05 +0300

    coupons page - first design and functionality

--- shabusionic/src/app/app.component.ts
+++ shabusionic/src/app/app.component.ts
@@ -55,7 +55,7 @@ firebase.auth().onAuthStateChanged(user => {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
    
-        this.nav.push(page.component);
+        this.nav.setRoot(page.component);
    
   }
 
--- shabusionic/src/pages/my-coupons/my-coupons.ts
+++ shabusionic/src/pages/my-coupons/my-coupons.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the MyCouponsPage page.
@@ -14,11 +14,60 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyCouponsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  businesses: Array<string>;
+  chosenBusiness: number;
+  cardNumber: number;
+  phoneNumber: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
+    this.businesses = ['business1', 'business2', 'business3', 'business4', 'business5']
+    this.chosenBusiness = -1;
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MyCouponsPage');
+    console.log('ionViewDidLoad MyCouponsPage');  
   }
+
+
+  chooseBusiness(name : string){
+    for(var i=0; i<this.businesses.length; i++){
+      if(this.businesses[i] === name){
+        document.getElementsByClassName(name)[0].setAttribute("style", "border: 4px solid #606060");
+        this.chosenBusiness = i;
+      }
+      else if(this.businesses[i] !== name)
+        document.getElementsByClassName(this.businesses[i])[0].setAttribute("style", "border: none");
+
+    }
+  }
+
+
+  rideRegistration(){
+    if(this.chosenBusiness === -1){
+      let failed = this.alertCtrl.create({
+        title: '<font color="red">!שגיאה</font>',
+        message: 'אנא בחר עסק מבוקש',
+        buttons: [{text: 'סגור'}]
+      });
+      failed.present();
+    }
+
+    else{
+      let confirm = this.alertCtrl.create({
+        title: 'הרישום בוצע',
+        message: this.businesses[this.chosenBusiness] + ' :עסק' + '<p>' + this.cardNumber + ' :מספר כרטיס' 
+                  + '<p>' + this.phoneNumber + ' :מספר טלפון',
+        buttons: ['אישור']
+      });
+      confirm.present();
+    }
+  }
+
+
+
+
+
+
 
 }
